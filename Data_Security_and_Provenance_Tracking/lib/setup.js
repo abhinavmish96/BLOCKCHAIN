094ee677342c053fb4c7f73a3edae1e72d2e1e71
Abhinav Mishra <abhinavmish96@gmail.com>
2019-09-09 08:23:46 +0530

    smart-contract

--- Data_Security_and_Provenance_Tracking/lib/setup.js
+++ Data_Security_and_Provenance_Tracking/lib/setup.js
@@ -2,4 +2,20 @@
 * Setup the demo
 * @param {org.hcsc.network.SetupDemo} setupDemo - the SetupDemo transaction
 * @transaction
-*/
+*/function setupDemo(setupDemo) {    
+    var factory = getFactory();
+    var NS = 'org.hcsc.network';
+    var roles = ['suppliers', 'manufacturers', 'distributors', 'retailers', 'customers'];
+    var members = {};
+    
+    for (var role in setupDemo) {
+      var type = (role.charAt(0).toUpperCase() + role.slice(1)).slice(0, -1);
+      if (setupDemo[role] && roles.indexOf(role) !== -1) {
+          members[role] = [];
+          setupDemo[role].forEach(function(participant) {
+              var newRole = factory.newResource(NS, type, participant.tradeId);
+              newRole.companyName = participant.companyName;
+              members[role].push(newRole);
+          });      
+      } 
+    }   
